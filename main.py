0c76372081d3c64ad141542ca5947b20459f10bf
amelieBNPP <amelie.noury@bnpparibas.com>
2021-05-10 21:49:14 +0200

    :feat: new feature: add brut force algorithm

--- main.py
+++ main.py
@@ -3,21 +3,35 @@
 Created on Mar 31st
 @author: Amelie Noury
 """
-from pandas.core.frame import DataFrame
 from script.data_treatments import read_file, write_file
+from script.timer import TimeToCompute
+from script.brutforce import brutforce
 import pandas as pd
 
 def main():
     
-    nb_share = 20
+    nb_share = 7
     data = read_file()
-    input_share_name = DataFrame(data['share_name'][0:nb_share])
-    input_data_prices = DataFrame(data['share_price'][0:nb_share])
-    input_data_yield = DataFrame([
+    input_share_name = data['share_name'][0:nb_share]
+    input_data_prices = data['share_price'][0:nb_share]
+    input_data_yield = pd.Series([
         float(dta.replace("%","")) for dta in data['share_return'][0:nb_share]
     ])
-    
+
     portfolio_capacity = 500
+
+    # brut_force with loops
+    timer_brut_force = TimeToCompute()
+    timer_brut_force.start()
+    result_brut_force = brutforce(input_share_name, input_data_prices, input_data_yield, portfolio_capacity)
+    timer_brut_force.end()
+
+    print(f'--------------------SOLUTIONS-----------------------------')
+ 
+    print(f'the brut force program with {len(input_share_name)} shares and a portfolio capacity of {portfolio_capacity} euros: ')
+    print(result_brut_force)
+    print(f'the brut force programme with itertools run during {timer_brut_force.time_pass()} sec \n')
+
 
 
 main()
--- script/data_treatments.py
+++ script/data_treatments.py
@@ -5,12 +5,7 @@ import os
 def read_file():
     colnames = ['share_name', 'share_price', 'share_return']
     data  = pd.read_csv("./data/Share_prices.csv", names = colnames, header=0)
-    shares_detail = {
-        'share_name': data['share_name'],
-        'share_price': data['share_price'],
-        'share_return': data['share_return']
-    }
-    return shares_detail
+    return data
 
 def write_file(file_to_save, overwrite=True):
     if not os.path.exists("./solutions/brut_force.csv"):
