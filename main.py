dcfdeddff9a7820c405b91071372f7c2317c39be
amelieBNPP <amelie.noury@bnpparibas.com>
2021-05-10 20:30:32 +0200

    :wrench: refactor: transform data to dataframe

--- main.py
+++ main.py
@@ -3,16 +3,19 @@
 Created on Mar 31st
 @author: Amelie Noury
 """
+from pandas.core.frame import DataFrame
+from script.data_treatments import read_file, write_file
+import pandas as pd
 
 def main():
     
     nb_share = 20
     data = read_file()
-    input_share_name = data['share_name'][0:nb_share]
-    input_data_prices = data['share_price'][0:nb_share]
-    input_data_yield = [
+    input_share_name = DataFrame(data['share_name'][0:nb_share])
+    input_data_prices = DataFrame(data['share_price'][0:nb_share])
+    input_data_yield = DataFrame([
         float(dta.replace("%","")) for dta in data['share_return'][0:nb_share]
-    ]
+    ])
     
     portfolio_capacity = 500
 
